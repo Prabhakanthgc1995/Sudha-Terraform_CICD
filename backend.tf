b3d80ad008f5df5304540f5199630378e7f4c50b
nikhilpatil027 <nikhilrpatil027@gmail.com>
2025-03-09 20:51:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "tfbucket049884"
+    bucket = "terraform-bucket-nikhil"
     key = "remote.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 }
