30d0f782ff7ccee1492e155fbfc9971ab5f5d6d0
Sudha G C <158569395+Sudha2804@users.noreply.github.com>
2025-03-10 08:21:06 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
 #security group
-resource "aws_security_group" "webserver_access" {
-        name = "webserver_access"
+resource "aws_security_group" "webserver_access2" {
+        name = "webserver_access2"
         description = "allow ssh and http"
 
         ingress {
@@ -33,7 +33,7 @@ resource "aws_instance" "ourfirst" {
   instance_type          = "t2.micro"
   user_data              = filebase64("install_ansible.sh")
   vpc_security_group_ids = [aws_security_group.webserver_access.id]  # Correct reference
-  key_name               = "ubuntu"
+  key_name               = "terraform"
   
   tags = {
     Name      = "ec2-Terraform"
